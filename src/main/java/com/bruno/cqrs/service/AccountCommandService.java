ae3d40cf0d52f22e4c19aab3127fcfde7b9bba79
Bruno Ramalho <bramalho@divante.pl>
2021-04-03 19:04:49 +0200

    service

---
+++ src/main/java/com/bruno/cqrs/service/AccountCommandService.java
@@ -0,0 +1,44 @@
+package com.bruno.cqrs.service;
+
+import com.bruno.cqrs.command.CreateAccountCommand;
+import com.bruno.cqrs.command.CreditMoneyCommand;
+import com.bruno.cqrs.command.DebitMoneyCommand;
+import com.bruno.cqrs.dto.AccountCreationDTO;
+import com.bruno.cqrs.dto.MoneyAmountDTO;
+import com.bruno.cqrs.entity.BankAccount;
+import lombok.AllArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import static com.bruno.cqrs.service.ServiceUtils.formatUuid;
+
+@Service
+@AllArgsConstructor
+public class AccountCommandService {
+    private final CommandGateway commandGateway;
+
+    public CompletableFuture<BankAccount> createAccount(AccountCreationDTO creationDTO) {
+        return this.commandGateway.send(new CreateAccountCommand(
+                UUID.randomUUID(),
+                creationDTO.getInitialBalance(),
+                creationDTO.getOwner()
+        ));
+    }
+
+    public CompletableFuture<String> creditMoneyToAccount(String accountId, MoneyAmountDTO moneyCreditDTO) {
+        return this.commandGateway.send(new CreditMoneyCommand(
+                formatUuid(accountId),
+                moneyCreditDTO.getAmount()
+        ));
+    }
+
+    public CompletableFuture<String> debitMoneyFromAccount(String accountId, MoneyAmountDTO moneyDebitDTO) {
+        return this.commandGateway.send(new DebitMoneyCommand(
+                formatUuid(accountId),
+                moneyDebitDTO.getAmount()
+        ));
+    }
+}
---
+++ src/main/java/com/bruno/cqrs/service/AccountQueryService.java
@@ -0,0 +1,38 @@
+package com.bruno.cqrs.service;
+
+import com.bruno.cqrs.entity.BankAccount;
+import com.bruno.cqrs.query.FindBankAccountQuery;
+import lombok.AllArgsConstructor;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.messaging.Message;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.bruno.cqrs.service.ServiceUtils.formatUuid;
+
+@Service
+@AllArgsConstructor
+public class AccountQueryService {
+    private final QueryGateway queryGateway;
+    private final EventStore eventStore;
+
+    public CompletableFuture<BankAccount> findById(String accountId) {
+        return this.queryGateway.query(
+                new FindBankAccountQuery(formatUuid(accountId)),
+                ResponseTypes.instanceOf(BankAccount.class)
+        );
+    }
+
+    public List<Object> listEventsForAccount(String accountId) {
+        return this.eventStore
+                .readEvents(formatUuid(accountId).toString())
+                .asStream()
+                .map(Message::getPayload)
+                .collect(Collectors.toList());
+    }
+}
