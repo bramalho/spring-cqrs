75d48885ef83a1716b129254399b10236eb8cd54
Bruno Ramalho <bramalho@divante.pl>
2021-04-03 19:05:03 +0200

    query

---
+++ src/main/java/com/bruno/cqrs/repository/BankAccountRepository.java
@@ -0,0 +1,12 @@
+package com.bruno.cqrs.repository;
+
+import com.bruno.cqrs.entity.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> {
+
+}
